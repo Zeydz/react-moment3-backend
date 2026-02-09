b0bddae652bf1d80f9c281481e3069b7940f6ac3
Zeydz <39154045+Zeydz@users.noreply.github.com>
2026-02-09 14:21:00 +0100

    LOGIN & REGISTER implemented with httpCookie, JWT and BCRYPT with test route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,7 @@ import { prisma } from "../plugins/db";
 import bcrypt from "bcrypt";
 
 export default async function authRoutes(fastify: FastifyInstance) {
+  /* REGISTER USER */
   fastify.post("/auth/register", async (request, reply) => {
     try {
       const { username, password } = request.body as {
@@ -28,12 +29,58 @@ export default async function authRoutes(fastify: FastifyInstance) {
           passwordHash,
         },
       });
-      return reply
-        .status(201)
-        .send({
-          message: "User created",
-          user: { id: user.id, username: user.username },
-        });
+      return reply.status(201).send({
+        message: "User created",
+        user: { id: user.id, username: user.username },
+      });
+    } catch (error) {
+      console.error(error);
+      return reply.status(500).send({ error: "Internal server error" });
+    }
+  });
+
+  /* LOGIN USER */
+  fastify.post("/auth/login", async (request, reply) => {
+    try {
+      const { username, password } = request.body as {
+        username: string;
+        password: string;
+      };
+
+      /* Get user */
+      const user = await prisma.user.findUnique({ where: { username } });
+
+      if (!user) {
+        return reply
+          .status(400)
+          .send({ error: "Invalid username or password" });
+      }
+
+      /* Check password */
+      const isValidPassword = await bcrypt.compare(password, user.passwordHash);
+      if (!isValidPassword) {
+        return reply
+          .status(400)
+          .send({ error: "Invalid username or password" });
+      }
+
+      /* Create JWT */
+      const token = fastify.jwt.sign({
+        userId: user.id,
+        username: user.username,
+      });
+      /* Set cookie */
+      reply.setCookie("token", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        maxAge: 60 * 60,
+      });
+
+      return reply.send({
+        message: "Login successful",
+        user: { id: user.id, username: user.username },
+      });
     } catch (error) {
       console.error(error);
       return reply.status(500).send({ error: "Internal server error" });
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,54 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import authRoutes from "./routes/auth";
+import fastifyCookie from "@fastify/cookie";
+import fastifyJwt from "@fastify/jwt";
 
 const app = fastify({
-    logger: true
-})
-
-app.get('/', async function (request, reply) {
-    return {hello: 'world'}
+  logger: true,
 });
+
+app.register(fastifyCookie);
+
+/* Create JWT */
+app.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET!,
+  cookie: {
+    cookieName: "token",
+    signed: false,
+  },
+  sign: {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
+});
+
+/* Authenticate decorator, since Fastify doesnt provide authentication strategies  */
+app.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
+  try {
+    await request.jwtVerify();
+  } catch (error) {
+    return reply.status(401).send({ error: "Unauthorized" });
+  }
+});
+
+app.get("/", async function (request, reply) {
+  return { hello: "world" };
+});
+
+/* GUARDED ROUTE TEST */
+app.get('/me', { preValidation: [app.authenticate]}, async (request, reply) => {
+    return { user: request.user}
+})
 
 app.register(authRoutes);
 
+/* Start app */
 const start = async () => {
-    try {
-        await app.listen({port: 3000})
-    } catch (error) {
-        app.log.error(error);
-        process.exit(1);
-    }
-}
+  try {
+    await app.listen({ port: 3000 });
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+};
 
 start();
