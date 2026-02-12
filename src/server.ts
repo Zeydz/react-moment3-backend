6f7175af0b88e6609ac216cd2db2cda618340bf7
Zeydz <39154045+Zeydz@users.noreply.github.com>
2026-02-12 10:49:48 +0100

    chore: bind to network & port swap

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,9 @@ app.register(authRoutes);
 /* Start app */
 const start = async () => {
   try {
-    await app.listen({ port: 3000 });
+    const PORT = Number(process.env.PORT) || 3000;
+    await app.listen({ port: PORT, host: "0.0.0.0"});
+    console.log(`Server running on port ${PORT}`);
   } catch (error) {
     app.log.error(error);
     process.exit(1);
